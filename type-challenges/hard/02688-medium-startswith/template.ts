2e9e7a19cf8ee9446a5a97ed7e51a92abbe6680b
chen <596487930@qq.com>
2022-05-25 09:20:51 +0800

    complete

--- type-challenges/hard/02688-medium-startswith/template.ts
+++ type-challenges/hard/02688-medium-startswith/template.ts
@@ -1,1 +1,6 @@
-type StartsWith<T extends string, U extends string> = any
+// type StartsWith<T extends string, U extends string> = T extends `${infer TL}${infer TR}` ? U extends `${infer UL}${infer UR}` ? TL extends UL ? StartsWith<TR, UR> extends any ? U extends '' ? true : false : false : false : false : false
+
+
+type StartsWith<T extends string, U extends string> = T extends `${U}${infer P}`
+  ? true
+  : false
