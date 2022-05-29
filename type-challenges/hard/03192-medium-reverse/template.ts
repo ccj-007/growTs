19e3c01beaa9ac86b4c19e4b15cc0333ca171a75
chen <596487930@qq.com>
2022-05-29 13:03:19 +0800

    complete

--- type-challenges/hard/03192-medium-reverse/template.ts
+++ type-challenges/hard/03192-medium-reverse/template.ts
@@ -1,1 +1,6 @@
-type Reverse<T> = any
+// your answers
+type Reverse<T, R extends any[]=[]> = T extends [infer P, ...infer U]
+  ? Reverse<U, [P, ...R]>
+  : R
+
+//这里的R代表保存的数据，P的每次等同于push进数据，如果没数据，返回R
--- type-challenges/hard/03196-medium-flip-arguments/template.ts
+++ type-challenges/hard/03196-medium-flip-arguments/template.ts
@@ -1,1 +1,7 @@
-type FlipArguments<T> = any
+type Reverse2<X extends any[]> = X extends [infer F, ...infer R]
+  ? [...Reverse<R>, F]
+  : X
+
+type FlipArguments<T> = T extends (...args: [...infer A]) => infer R
+  ? (...args: Reverse2<[...A]>) => R
+  : never
