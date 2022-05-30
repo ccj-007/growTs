c944b14a6b44ceaa33b484dc10634eadc7034f03
chen <596487930@qq.com>
2022-05-30 08:46:21 +0800

    complete

--- type-challenges/hard/03243-medium-flattendepth/template.ts
+++ type-challenges/hard/03243-medium-flattendepth/template.ts
@@ -1,1 +1,9 @@
-type FlattenDepth = any
+// type FlattenDepth<T extends any[],  A extends any[] = []> = T extends [infer L, ...infer R] ? L extends number[] ? 
+// FlattenDepth<R,  [...L, ...A]> : FlattenDepth<R,  [L, ...A]> : A
+type FlattenDepth<T, P extends number = 1, A extends any[] = []> = A['length'] extends P
+? T
+: T extends [infer F, ...infer R]
+  ? F extends any[]
+    ? [...FlattenDepth<F, P, [0, ...A]>, ...FlattenDepth<R, P, A>]
+    : [F, ...FlattenDepth<R, P, A>]
+  : T;
--- type-challenges/hard/03326-medium-bem-style-string/template.ts
+++ type-challenges/hard/03326-medium-bem-style-string/template.ts
@@ -1,1 +1,8 @@
-type BEM<B extends string, E extends string[], M extends string[]> = any
+// your answers
+type BEM<B extends string, E extends string[], M extends string[]> = E['length'] extends 0
+  ? M['length'] extends 0
+    ? B
+    : `${B}--${M[number]}`
+  : M['length'] extends 0
+    ? `${B}__${E[number]}`
+    : `${B}__${E[number]}--${M[number]}`
