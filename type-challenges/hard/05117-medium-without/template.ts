9e75f8682b6c908f377ed29fd62b11759f99b567
chen <596487930@qq.com>
2022-06-04 09:40:48 +0800

    complete

--- type-challenges/hard/05117-medium-without/template.ts
+++ type-challenges/hard/05117-medium-without/template.ts
@@ -1,1 +1,6 @@
-type Without<T, U> = any
+type Without<T extends unknown[], U> =
+    T extends [infer E, ...infer Rest]
+    ? E extends Exclude<T[number], U extends unknown[] ? U[number] : U>    //将U从数组中排除在T中，得到number，如果E是在里面的就是我需要的， Exclude返回的是类型
+    ? [E, ...Without<Rest, U>]  //这里是我需要的
+    : Without<Rest, U>  //说明这个number是需要排除在T中的
+    : T
--- type-challenges/hard/05140-medium-trunc/template.ts
+++ type-challenges/hard/05140-medium-trunc/template.ts
@@ -1,1 +1,3 @@
-type Trunc = any
+type Trunc<T extends number | string> = `${T}` extends `${infer N}.${any}` ? N : `${T}`
+
+//numebr如何转string，通过`${T}`
