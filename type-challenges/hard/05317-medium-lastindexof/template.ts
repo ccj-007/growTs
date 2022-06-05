2ac9fefb127ede858b29f9f09935ade1b5ddd6bb
chen <596487930@qq.com>
2022-06-06 07:16:10 +0800

    complete

--- type-challenges/hard/05317-medium-lastindexof/template.ts
+++ type-challenges/hard/05317-medium-lastindexof/template.ts
@@ -1,1 +1,5 @@
-type LastIndexOf<T, U> = any
+type LastIndexOf<T extends any[], U> = T extends [...infer I,infer L]? L extends U ? I['length']: LastIndexOf<I,U> : -1;
+
+
+//只要是数组想获取下标，直接T['length']
+//infer的位置及其灵活
--- type-challenges/hard/05360-medium-unique/template.ts
+++ type-challenges/hard/05360-medium-unique/template.ts
@@ -1,1 +1,8 @@
-type Unique<T> = any
+type Unique<T extends unknown[], R extends unknown[] = []> = T extends [
+  infer A,
+  ...infer Rest
+]
+  ? Unique<Rest, A extends R[number] ? R : [...R, A]>
+  : R;
+
+  //通过infer解构出来然后通过，在空数组通过三元表达式来判断是否push
