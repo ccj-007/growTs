9a528e2e57dcf9b6a5c19b0816f8d06b739ca9bc
chen <596487930@qq.com>
2022-06-08 21:37:41 +0800

    complete

--- type-challenges/hard/08987-medium-subsequence/template.ts
+++ type-challenges/hard/08987-medium-subsequence/template.ts
@@ -1,1 +1,3 @@
-type Subsequence<T extends any[]> = any
+type Subsequence<T extends any[],Prefix extends any[] = []> = T extends [infer F,...infer R] ? Subsequence<R, Prefix | [...Prefix,F]>:Prefix
+
+//核心通过infer数组的是否判断, 将profix的状态不断更新，递归返回不断新增返回的prefix的union类型
