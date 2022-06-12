1987c43807b654290f3d2ea03d074b141db3afd7
chen <596487930@qq.com>
2022-06-12 12:23:26 +0800

    complete

--- type-challenges/hard/00055-hard-union-to-intersection/template.ts
+++ type-challenges/hard/00055-hard-union-to-intersection/template.ts
@@ -1,1 +1,9 @@
-type UnionToIntersection<U> = any
+type UnionToIntersection<U> = (U extends any ? (arg: U) => any : never) extends ((arg: infer I) => void) ? I : never
+
+type A = 'foo' | 42 | true
+
+type B = A extends any ? (arg: A) => any : never // (arg: A) => any 
+
+type C = B extends ((arg: infer I) => void) ? I : never //  'foo' | 42 | true
+
+type D = 'foo' & 2 & true // never
